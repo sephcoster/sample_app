142e01c5b6d5e6572369555425c514129e4eec42
Seph <sailfast@gmail.com>
2012-05-19 14:08:27 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+	it "should have the content 'Sample App'" do
+	  visit '/static_pages/home'
+	  page.should have_selector('h1', :text =>'Sample App')
+	end
+	
+	it "should have the title 'Home'" do
+	  visit '/static_pages/home'
+	  page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Home")
+	end
+
+  end
+
+  describe "Help page" do
+	it "should have the content 'Help'" do
+	  visit '/static_pages/help'
+	  page.should have_selector('h1', :text =>'Help')
+	end
+	
+	it "should have the title 'Help'" do
+	  visit '/static_pages/help'
+	  page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Help")
+	end
+
+  end
+
+
+  describe "About page" do
+	it "should have the content 'About Us'" do
+	  visit '/static_pages/about'
+	  page.should have_selector('h1', :text =>'About Us')
+	end
+	
+	it "should have the title 'About Us'" do
+	  visit '/static_pages/about'
+	  page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | About Us")
+	end
+  end
+
+  describe "Contact page" do
+	it "should have the content 'Contact'" do
+	  visit '/static_pages/contact'
+	  page.should have_selector('h1', :text =>'Contact')
+	end
+	
+	it "should have the title 'Contact'" do
+	  visit '/static_pages/contact'
+	  page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Contact")
+	end
+
+  end
+
+
+end
+
